02b719907258b02711c08df2c437b152c6bd6e01
SanthiyaA16 <santhiyaa1601@gmail.com>
2025-09-09 19:52:20 +0530

    Create Sum of Elements in Array 6.java

---
+++ Arrays/1D Array/Sum of Elements in Array 6.java
@@ -0,0 +1,19 @@
+//Write a program to read n integers into an array and print the sum of all the elements.
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        int sum=0;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            sum+=arr[i];
+        }
+        System.out.println(sum);
+    }
+}
