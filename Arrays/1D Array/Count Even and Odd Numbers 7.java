9aa08da394dd62ef6dc288d70b3e1dc6bb4fb28d
SanthiyaA16 <santhiyaa1601@gmail.com>
2025-09-09 19:55:27 +0530

    Create Count Even and Odd Numbers 7.java

---
+++ Arrays/1D Array/Count Even and Odd Numbers 7.java
@@ -0,0 +1,26 @@
+//Count how many even and odd numbers are present in the given array .
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        int even=0;
+        int odd=0;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            if(arr[i]%2==0){
+                even++;
+            }
+            else{
+                odd++;
+            }
+        }
+        System.out.println(even);
+         System.out.println(odd);
+    }
+}
